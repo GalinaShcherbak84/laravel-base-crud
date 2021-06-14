24b1b21f8524318e58bbf0da1cc1cb88cf98d848
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-14 16:47:27 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<nav class="nav mb-3 mt-3">
+    <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </nav>

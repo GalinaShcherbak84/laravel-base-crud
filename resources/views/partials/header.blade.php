93aba1d95c132e1e03238e662d8aa576f20b4815
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-15 15:01:13 +0200

    class active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,11 @@
+{{-- {{Route::currentRouteName()}} {{Request::route()->getName()}} --}}
 <nav class="nav mb-3 mt-3">
-    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
-    <a class="nav-link" href="{{route('comics.index')}}">Comics</a>
+    <a class="nav-link @if(Route::currentRouteName()== 'home') active @endif" aria-current="page"
+     href="{{route('home')}}">
+      Home
+    </a>
+    <a class="nav-link @if(Route::currentRouteName()== 'comics.index') active @endif" 
+      href="{{route('comics.index')}}">
+      Comics
+    </a>
   </nav>

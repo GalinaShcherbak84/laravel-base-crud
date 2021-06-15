9e2a89a9536275121a20ccd467e710a284c50b9a
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-15 18:57:17 +0200

    update navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('content')
 <div class="container">
-  <h1>The largest comics archive here...</h1>
+  <h1>The largest comics archive is here...</h1>
 </div>
 @endsection
